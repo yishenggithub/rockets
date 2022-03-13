02d9c2429bad4ebfee45182dd13246a3095d8829
易晨 <easom@yishengs-MacBook-Pro.local>
2022-03-13 17:13:29 +0800

    init

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/yishenggithub/rockets-go-tutorial
+
+go 1.17
+
+require (
+	gioui.org v0.0.0-20211113093644-40bc2e1f88b8 // indirect
+	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
+	gioui.org/shader v1.0.6 // indirect
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/esimov/caire v1.4.2 // indirect
+	github.com/esimov/pigo v1.4.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/exp v0.0.0-20210722180016-6781d3edade3 // indirect
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/esimov/caire"
+	"github.com/pkg/errors"
+)
+
+const (
+	IMAGE_URL string = "https://bit.ly/2QGPDkr"
+)
+
+func ContentAwareResize(url string) ([]byte, error) {
+	fmt.Printf("Download starting for url %s\n", url)
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to read the image")
+	}
+	defer response.Body.Close()
+	converted := &bytes.Buffer{}
+	fmt.Printf("Download complete %s", url)
+	shrinkFactor := 30
+	fmt.Printf("Resize in progress %s, shrinking width by %d percent...\n", url, shrinkFactor)
+	p := &caire.Processor{
+		NewWidth:   shrinkFactor,
+		Percentage: true,
+	}
+	err = p.Process(response.Body, converted)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to apply seam carving to the image")
+	}
+	fmt.Printf("Seam carving completed for %s\n", url)
+	return converted.Bytes(), nil
+}
+
+func main() {
+	fmt.Println("Ready for liftoff! Checkout http://localhost:3000/occupymars")
+	http.HandleFunc("/occupymars", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("resize") > "" {
+			resized, err := ContentAwareResize(IMAGE_URL)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "image/jpeg")
+			io.Copy(w, bytes.NewReader(resized))
+		} else {
+			fmt.Fprintf(w, "<html><div>Original image:</div> <img src=\"%s\" /><br/><a href=\"?resize=1\">Resize using Seam Carving</a></html>", IMAGE_URL)
+		}
+	})
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
